216d9ada3e946d4ea17a17f5b956acd2b217f773
NoobinOut <110687587+NoobinOut@users.noreply.github.com>
2026-01-12 18:22:30 -0500

    Add commands that need to be programmed

---
+++ src/main/java/frc/robot/commands/ClimbLevel3Command.java
@@ -0,0 +1,7 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class ClimbLevel3Command extends Command {
+    // TODO: make this; command that makes us climb to level 3
+}
---
+++ src/main/java/frc/robot/commands/UnClimbCommand.java
@@ -0,0 +1,7 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class UnClimbCommand extends Command {
+    // TODO: make this; command that makes us drop from level 1 to the carpet.
+}
