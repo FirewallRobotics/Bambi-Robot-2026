85df0a35465ef33fa378b831e0645f3336545e19
Team-Laptop <151387613+Cpixel151@users.noreply.github.com>
2026-01-15 16:03:02 -0500

    intake branch

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -1,7 +1,31 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSubsystem;
 
 public class IntakeCommand extends Command {
-    // TODO: Make this; should intake fuel
+   
+    private final IntakeSubsystem m_IntakeSubsystem;
+
+    public IntakeCommand(IntakeSubsystem i_Subsystem){
+        m_IntakeSubsystem=i_Subsystem;
+    }
+    
+    @Override
+    public void initialize(){}
+
+    @Override
+    public void execute(){
+        m_IntakeSubsystem.StartIntake(1.0);
+    }
+   
+    @Override
+    public void end(boolean interupted){
+        m_IntakeSubsystem.StopIntake();
+    }
+
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
 }
---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSubsystem extends SubsystemBase {
+
+    private final Spark motor;
+
+    public IntakeSubsystem(){
+       motor = new Spark(0); 
+    }
+  
+    public void StartIntake(Double speed){
+        motor.set(speed);
+    }
+
+    public void StopIntake(){
+        motor.set(0);
+    }
+}
