8e84febea832665165ff934130973a2dac39ea0d
Team-Laptop <151387613+Cpixel151@users.noreply.github.com>
2026-01-17 15:58:53 -0500

    Shooter add

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,12 @@ public final class Constants {
   public static class VisionSubsystemConstants {
     public static final String limelightName = "limelight-cyclops";
   }
+
+  public static class ArmConstants {
+
+    public static final double kSVolts = 0.11356;
+    public static final double kGVolts = 0.29175;
+    public static final double kVVoltSecondPerRad = 1.5928;
+    public static final double kAVoltSecondSquaredPerRad = 0.030171;
+  }
 }
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,21 +1,40 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ArmConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
 
-  private final Spark motor;
+  private final Spark arm_motor;
+  private final Spark intake_motor;
+  private ArmFeedforward armFeedfoward;
 
   public IntakeSubsystem() {
-    motor = new Spark(0);
+    intake_motor = new Spark(1);
+    arm_motor = new Spark(2);
+    armFeedfoward =
+        new ArmFeedforward(
+            ArmConstants.kSVolts,
+            ArmConstants.kGVolts,
+            ArmConstants.kVVoltSecondPerRad,
+            ArmConstants.kAVoltSecondSquaredPerRad);
+  }
+
+  public void holdUp(Double position) {
+    armFeedfoward.calculate(position, 2, 3);
+  }
+
+  public void angleArm(Double speed) {
+    arm_motor.set(speed);
   }
 
   public void StartIntake(Double speed) {
-    motor.set(speed);
+    intake_motor.set(speed);
   }
 
   public void StopIntake() {
-    motor.set(0);
+    intake_motor.set(0);
   }
 }
