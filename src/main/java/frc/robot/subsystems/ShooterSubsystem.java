c69a6eb0ef768b652ba77166fa3ab7471eaf0859
Cpixel151 <milostories151@gmail.com>
2026-02-05 18:41:50 -0500

    Working

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -23,23 +23,20 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ShooterSubsystem extends SubsystemBase {
 
   private final SparkFlex shootMotorTop;
-  // private final SparkFlex leftFollow;
+  private final SparkFlex shootFollowTop;
   private final SparkFlex shootMotorBottom;
-  // private final SparkFlex rightFollow;
+  private final SparkFlex shootFollowBottom;
 
-  // private final SparkFlexConfig lFollowerConfig;
-  // private final SparkFlexConfig rFollowerConfig;
+  private final SparkFlexConfig tFollowerConfig;
+  private final SparkFlexConfig bFollowerConfig;
   private final SparkFlexConfig tShootConfig;
   private final SparkFlexConfig bShootConfig;
 
   // private final PIDController finalShootPID;
-  private SimpleMotorFeedforward topMotorFeedforward;
-  private SimpleMotorFeedforward bottomMotorFeedforward;
-  private final RelativeEncoder tShootEncoder;
-  private final RelativeEncoder bShootEncoder;
+  
 
-  private final PIDController tShootPID;
   private SparkClosedLoopController tShootClosedLoopController;
+  private SparkClosedLoopController bSparkClosedLoopController;
 
   private double setVelocity;
   
@@ -51,43 +48,28 @@ public class ShooterSubsystem extends SubsystemBase {
     SmartDashboard.putNumber("Velocity Top", 0);
     SmartDashboard.putNumber("Velocity Bottom", 0);
     SmartDashboard.putNumber("Current Limit", 0);
-    setVelocity = 1200;
+    setVelocity = 2100;
+    double p = 0.0005;
+    double i = 0.0000005;
 
     shootMotorTop = new SparkFlex(14, MotorType.kBrushless);
     shootMotorBottom = new SparkFlex(15, MotorType.kBrushless);
-    // leftFollow = new SparkFlex(0, MotorType.kBrushless);
-    // rightFollow = new SparkFlex(0, MotorType.kBrushless);
+    shootFollowTop = new SparkFlex(13, MotorType.kBrushless);
+    shootFollowBottom = new SparkFlex(16, MotorType.kBrushless);
 
-    // lFollowerConfig = new SparkFlexConfig();
-    // rFollowerConfig = new SparkFlexConfig();
+    bFollowerConfig = new SparkFlexConfig();
+    tFollowerConfig = new SparkFlexConfig();
     tShootConfig = new SparkFlexConfig();
     bShootConfig = new SparkFlexConfig();
 
     tShootClosedLoopController = shootMotorTop.getClosedLoopController();
-
-    topMotorFeedforward = new SimpleMotorFeedforward(1, 3, 2);
-    bottomMotorFeedforward = new SimpleMotorFeedforward(1, 3, 2);
-    
-    // finalShootPID = new PIDController(1, 1, 1);
-    // kickMotor = new SparkFlex(0, MotorType.kBrushless);
-    
-    tShootPID = new PIDController(0.01, 0, 0);
-    tShootPID.setTolerance(100);
+    bSparkClosedLoopController = shootMotorBottom.getClosedLoopController();
 
     
 
-    tShootEncoder = shootMotorTop.getEncoder();
-    bShootEncoder = shootMotorBottom.getEncoder();
+    tShootConfig.smartCurrentLimit(30);
+    bShootConfig.smartCurrentLimit(30);
 
-    // lFollowerConfig.inverted(true);
-    // lFollowerConfig.follow(shootMotorLeft);
-    // rFollowerConfig.inverted(true);
-    // rFollowerConfig.follow(shootMotorRight);
-
-    tShootConfig.smartCurrentLimit(20);
-    bShootConfig.smartCurrentLimit(20);
-
-    // tShootConfig.inverted(true);
     tShootConfig.encoder.positionConversionFactor(1);
     tShootConfig.encoder.positionConversionFactor(1);
 
@@ -95,8 +77,26 @@ public class ShooterSubsystem extends SubsystemBase {
         .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
         // Set PID values for position control. We don't need to pass a closed loop
         // slot, as it will default to slot 0.
-        .p(0.001)
-        .i(0.0000000001)
+        .p(p)
+        .i(i)
+        .d(0)
+        .outputRange(-1, 1)
+        // Set PID values for velocity control in slot 1
+        .p(0.0001, ClosedLoopSlot.kSlot1)
+        .i(0, ClosedLoopSlot.kSlot1)
+        .d(0, ClosedLoopSlot.kSlot1)
+        .outputRange(-1, 1, ClosedLoopSlot.kSlot1)
+        .feedForward
+          // kV is now in Volts, so we multiply by the nominal voltage (12V)
+          .kV(12.0 / 5767, ClosedLoopSlot.kSlot1);
+
+    
+    bShootConfig.closedLoop
+        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+        // Set PID values for position control. We don't need to pass a closed loop
+        // slot, as it will default to slot 0.
+        .p(p)
+        .i(i)
         .d(0)
         .outputRange(-1, 1)
         // Set PID values for velocity control in slot 1
@@ -116,12 +116,10 @@ public class ShooterSubsystem extends SubsystemBase {
         bShootConfig,
         com.revrobotics.ResetMode.kNoResetSafeParameters,
         com.revrobotics.PersistMode.kPersistParameters);
-    // leftFollow.configure(
-    //     lFollowerConfig,
+    // shootFollowTop.configure(tFollowerConfig,
     //     com.revrobotics.ResetMode.kNoResetSafeParameters,
     //     com.revrobotics.PersistMode.kPersistParameters);
-    // rightFollow.configure(
-    //     rFollowerConfig,
+    // shootFollowBottom.configure(bFollowerConfig,
     //     com.revrobotics.ResetMode.kNoResetSafeParameters,
     //     com.revrobotics.PersistMode.kPersistParameters);
   }
@@ -132,27 +130,8 @@ public class ShooterSubsystem extends SubsystemBase {
     // shootMotorBottom.set(-1);
     //shootMotorTop.set(1);
     tShootClosedLoopController.setSetpoint(setVelocity, ControlType.kVelocity);
-    //shootMotorTop.setVoltage(topMotorFeedforward.calculateWithVelocities(tShootEncoder.getVelocity(), setVelocity) + tShootPID.calculate(tShootEncoder.getVelocity(), setVelocity));
-
-    // shootMotorTop.setVoltage(
-    //     topMotorFeedforward.calculateWithVelocities(
-    //         tShootEncoder.getVelocity(), 600));
-    shootMotorBottom.setVoltage(
-        bottomMotorFeedforward.calculateWithVelocities(
-            bShootEncoder.getVelocity(), 600));
-    //Logger.getGlobal().log(Level.INFO, "Top V: " + tShootEncoder.getVelocity());
-    // shootMotorLeft.setVoltage(
-    //     motorFeedforward.calculate(
-    //             SmartDashboard.getNumber("Velocity", 0),
-    //             SmartDashboard.getNumber("Acceleration", 0))
-    //         + finalShootPID.calculate(
-    //             shootEncoder.getVelocity(), SmartDashboard.getNumber("Velocity", 0)));
-    // shootMotorRight.setVoltage(
-    //     motorFeedforward.calculate(
-    //             SmartDashboard.getNumber("Velocity", 0),
-    //             SmartDashboard.getNumber("Acceleration", 0))
-    //         + finalShootPID.calculate(
-    //             shootEncoder.getVelocity(), SmartDashboard.getNumber("Velocity", 0)));
+    bSparkClosedLoopController.setSetpoint(setVelocity, ControlType.kVelocity);
+    
   }
 
   // Used to kick the balls up from the storage up into the shooter
