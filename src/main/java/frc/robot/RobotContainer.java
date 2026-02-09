2f05fad2f6119b35e38b7746708d742df2345acd
Cpixel151 <Pixeldigit151@gmail.com>
2026-02-09 17:05:23 -0500

    that didn't work

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -105,7 +105,7 @@ public class RobotContainer {
             drivetrain.applyRequest(
                 () ->
                     face.withTargetDirection(
-                        new Rotation2d(VisionSubsystem.getAngleToHUB(drivetrain) + 3.14))));
+                        new Rotation2d(VisionSubsystem.getAngleToHUB(drivetrain)))));
 
     // Run SysId routines when holding back/start and X/Y.
     // Note that each routine should be run exactly once in a single log.
