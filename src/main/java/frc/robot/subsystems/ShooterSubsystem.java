3406dca3120e371877c97bcc0a0575afd6d34daa
MatthewWiggs <matthews.pokemail151@gmail.com>
2026-03-01 16:53:59 -0500

    Prototype Starboard Motor

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -6,13 +6,24 @@ import com.revrobotics.spark.config.SparkFlexConfig;
 
 public class ShooterSubsystem {
   private final SparkFlex shooterTopPortMotor;
-
+  private final SparkFlex shooterTopStarboardMotor;
   private final SparkFlexConfig tPortConfig;
+  private final SparkFlexConfig tStarboardConfig;
 
   public ShooterSubsystem() {
     shooterTopPortMotor = new SparkFlex(1, MotorType.kBrushless);
-
+    shooterTopStarboardMotor = new SparkFlex(6, MotorType.kBrushless);
     tPortConfig = new SparkFlexConfig();
+    tStarboardConfig = new SparkFlexConfig();
+    tStarboardConfig.follow(1, true);
+    shooterTopPortMotor.configure(
+        tPortConfig,
+        com.revrobotics.ResetMode.kNoResetSafeParameters,
+        com.revrobotics.PersistMode.kPersistParameters);
+    shooterTopStarboardMotor.configure(
+        tStarboardConfig,
+        com.revrobotics.ResetMode.kNoResetSafeParameters,
+        com.revrobotics.PersistMode.kPersistParameters);
   }
 
   public void Shoot() {
