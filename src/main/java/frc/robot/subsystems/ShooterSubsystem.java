ea4cfd56f2e48c42ba62524e68c3ac359301031b
ItsYour14 <jzfortnite987@gmail.com>
2026-03-02 17:29:48 -0500

    prototype shooterBottomPortMotor

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -7,14 +7,21 @@ import com.revrobotics.spark.config.SparkFlexConfig;
 public class ShooterSubsystem {
   private final SparkFlex shooterTopPortMotor;
   private final SparkFlex shooterTopStarboardMotor;
+  private final SparkFlex shooterBottomPortMotor;
+
   private final SparkFlexConfig tPortConfig;
   private final SparkFlexConfig tStarboardConfig;
+  private final SparkFlexConfig bPortConfig;
 
   public ShooterSubsystem() {
     shooterTopPortMotor = new SparkFlex(1, MotorType.kBrushless);
     shooterTopStarboardMotor = new SparkFlex(6, MotorType.kBrushless);
+    shooterBottomPortMotor = new SparkFlex(4, MotorType.kBrushless);
+
     tPortConfig = new SparkFlexConfig();
     tStarboardConfig = new SparkFlexConfig();
+    bPortConfig = new SparkFlexConfig();
+
     tStarboardConfig.follow(1, true);
     shooterTopPortMotor.configure(
         tPortConfig,
@@ -24,15 +31,21 @@ public class ShooterSubsystem {
         tStarboardConfig,
         com.revrobotics.ResetMode.kNoResetSafeParameters,
         com.revrobotics.PersistMode.kPersistParameters);
+    shooterBottomPortMotor.configure(
+        bPortConfig,
+        com.revrobotics.ResetMode.kNoResetSafeParameters,
+        com.revrobotics.PersistMode.kPersistParameters);
   }
 
   public void Shoot() {
     shooterTopPortMotor.set(-0.3);
+    shooterBottomPortMotor.set(0.3);
   }
 
   public void Upward() {}
 
   public void StopShoot() {
     shooterTopPortMotor.set(0);
+    shooterBottomPortMotor.set(0);
   }
 }
