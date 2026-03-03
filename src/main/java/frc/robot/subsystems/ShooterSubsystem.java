c3d6743436bea6fe6fb3d5db9b59b75d680a6e9f
ItsYour14 <jzfortnite987@gmail.com>
2026-03-02 19:51:37 -0500

    added another motor and started PID

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkFlexConfig;
 
@@ -8,20 +11,51 @@ public class ShooterSubsystem {
   private final SparkFlex shooterTopPortMotor;
   private final SparkFlex shooterTopStarboardMotor;
   private final SparkFlex shooterBottomPortMotor;
+  private final SparkFlex kickerPortMotor;
+
+  private final SparkClosedLoopController topPortClosedLoopController;
+  private final SparkClosedLoopController bottomPorClosedLoopController;
 
   private final SparkFlexConfig tPortConfig;
   private final SparkFlexConfig tStarboardConfig;
   private final SparkFlexConfig bPortConfig;
+  private final SparkFlexConfig kickerPortConfig;
 
   public ShooterSubsystem() {
     shooterTopPortMotor = new SparkFlex(1, MotorType.kBrushless);
     shooterTopStarboardMotor = new SparkFlex(6, MotorType.kBrushless);
     shooterBottomPortMotor = new SparkFlex(4, MotorType.kBrushless);
+    kickerPortMotor = new SparkFlex(3, MotorType.kBrushless);
 
     tPortConfig = new SparkFlexConfig();
     tStarboardConfig = new SparkFlexConfig();
     bPortConfig = new SparkFlexConfig();
+    kickerPortConfig = new SparkFlexConfig();
 
+    tPortConfig.smartCurrentLimit(40);
+    bPortConfig.smartCurrentLimit(40);
+    topPortClosedLoopController = shooterTopPortMotor.getClosedLoopController();
+    bottomPorClosedLoopController = shooterBottomPortMotor.getClosedLoopController();
+
+    tPortConfig
+      .closedLoop
+      .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+      // Set PID values for position control. We don't need to pass a closed loop
+      // slot, as it will default to slot 0.
+      .p(0)
+      .i(0)
+      .d(0)
+      .outputRange(-1, 1);
+    bPortConfig
+      .closedLoop
+      .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+      // Set PID values for position control. We don't need to pass a closed loop
+      // slot, as it will default to slot 0.
+      .p(0)
+      .i(0)
+      .d(0)
+      .outputRange(-1, 1);
+    
     tStarboardConfig.follow(1, true);
     shooterTopPortMotor.configure(
         tPortConfig,
@@ -35,17 +69,22 @@ public class ShooterSubsystem {
         bPortConfig,
         com.revrobotics.ResetMode.kNoResetSafeParameters,
         com.revrobotics.PersistMode.kPersistParameters);
+    kickerPortMotor.configure(
+        kickerPortConfig, 
+        com.revrobotics.ResetMode.kNoResetSafeParameters,
+        com.revrobotics.PersistMode.kPersistParameters);
   }
 
   public void Shoot() {
-    shooterTopPortMotor.set(-0.3);
-    shooterBottomPortMotor.set(0.3);
+    //shooterTopPortMotor.set(-0.3);
+    topPortClosedLoopController.setSetPoint(2000, ControlType.kVelocity);
+    // shooterBottomPortMotor.set(0.3);
   }
 
   public void Upward() {}
 
   public void StopShoot() {
     shooterTopPortMotor.set(0);
-    shooterBottomPortMotor.set(0);
+    // shooterBottomPortMotor.set(0);
   }
 }
