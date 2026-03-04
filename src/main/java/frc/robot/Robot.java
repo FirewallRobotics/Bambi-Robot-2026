2272ed1e4b2a29185c6615dd4abac0be7b62c130
NoobinOut <110687587+NoobinOut@users.noreply.github.com>
2026-03-04 18:05:49 -0500

    More autos!

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -35,6 +35,7 @@ public class Robot extends TimedRobot {
 
     m_chooser.setDefaultOption("Shoot In Circle (Primary scorer, Start on Right)", "Grab N Go");
     m_chooser.addOption("Shoot, Empty depot, climb (Start on Left)", "Home Cycle");
+    m_chooser.addOption("Show Off, Around the world", "Show Off");
     m_chooser.addOption("Just Empty and Climb (Start on Left)", "Lets Hang");
     m_chooser.addOption("Move Forward (Debug)", "Just Forward");
     SmartDashboard.putData("Auto Chooser", m_chooser);
