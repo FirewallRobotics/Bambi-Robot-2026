7896fa20f1d160169d6ec409c4f1441206106d1d
ItsYour14 <jzfortnite987@gmail.com>
2026-03-05 19:49:28 -0500

    bottom pid + bug fuxes

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -9,33 +9,35 @@ import com.revrobotics.spark.config.SparkFlexConfig;
 
 public class ShooterSubsystem {
   private final SparkFlex shooterTopPortMotor;
-  private final SparkFlex shooterTopStarboardMotor;
+  // private final SparkFlex shooterTopStarboardMotor;
   private final SparkFlex shooterBottomPortMotor;
   private final SparkFlex kickerPortMotor;
 
   private final SparkClosedLoopController topPortClosedLoopController;
-  private final SparkClosedLoopController bottomPorClosedLoopController;
+  private final SparkClosedLoopController bottomPortClosedLoopController;
 
   private final SparkFlexConfig tPortConfig;
-  private final SparkFlexConfig tStarboardConfig;
+  // private final SparkFlexConfig tStarboardConfig;
   private final SparkFlexConfig bPortConfig;
   private final SparkFlexConfig kickerPortConfig;
 
   public ShooterSubsystem() {
     shooterTopPortMotor = new SparkFlex(1, MotorType.kBrushless);
-    shooterTopStarboardMotor = new SparkFlex(6, MotorType.kBrushless);
+    // shooterTopStarboardMotor = new SparkFlex(6, MotorType.kBrushless);
     shooterBottomPortMotor = new SparkFlex(4, MotorType.kBrushless);
     kickerPortMotor = new SparkFlex(3, MotorType.kBrushless);
 
     tPortConfig = new SparkFlexConfig();
-    tStarboardConfig = new SparkFlexConfig();
+    // tStarboardConfig = new SparkFlexConfig();
     bPortConfig = new SparkFlexConfig();
     kickerPortConfig = new SparkFlexConfig();
 
     tPortConfig.smartCurrentLimit(40);
     bPortConfig.smartCurrentLimit(40);
+    tPortConfig.encoder.positionConversionFactor(1);
+    bPortConfig.encoder.positionConversionFactor(1);
     topPortClosedLoopController = shooterTopPortMotor.getClosedLoopController();
-    bottomPorClosedLoopController = shooterBottomPortMotor.getClosedLoopController();
+    bottomPortClosedLoopController = shooterBottomPortMotor.getClosedLoopController();
 
     tPortConfig
         .closedLoop
@@ -45,27 +47,27 @@ public class ShooterSubsystem {
         .p(0.0000035)
         .i(0.1)
         .d(0)
-        //daniel's fault
+        // daniel's fault
         .outputRange(-1, 1);
     bPortConfig
         .closedLoop
         .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
         // Set PID values for position control. We don't need to pass a closed loop
         // slot, as it will default to slot 0.
-        .p(0.5)
-        .i(0)
+        .p(0.0000035)
+        .i(0.1)
         .d(0)
         .outputRange(-1, 1);
 
-    tStarboardConfig.follow(1, true);
+    // tStarboardConfig.follow(1, true);
     shooterTopPortMotor.configure(
         tPortConfig,
         com.revrobotics.ResetMode.kNoResetSafeParameters,
         com.revrobotics.PersistMode.kPersistParameters);
-    shooterTopStarboardMotor.configure(
-        tStarboardConfig,
-        com.revrobotics.ResetMode.kNoResetSafeParameters,
-        com.revrobotics.PersistMode.kPersistParameters);
+    // shooterTopStarboardMotor.configure(
+    // tStarboardConfig,
+    // com.revrobotics.ResetMode.kNoResetSafeParameters,
+    // com.revrobotics.PersistMode.kPersistParameters);
     shooterBottomPortMotor.configure(
         bPortConfig,
         com.revrobotics.ResetMode.kNoResetSafeParameters,
@@ -78,14 +80,17 @@ public class ShooterSubsystem {
 
   public void Shoot() {
     // shooterTopPortMotor.set(-0.3);
-    topPortClosedLoopController.setSetpoint(-2000, ControlType.kVelocity);
+    topPortClosedLoopController.setSetpoint(2000, ControlType.kVelocity);
+    bottomPortClosedLoopController.setSetpoint(-2000, ControlType.kVelocity);
     // shooterBottomPortMotor.set(0.3);
   }
 
   public void Upward() {}
 
   public void StopShoot() {
-    shooterTopPortMotor.set(0);
+    // shooterTopPortMotor.set(0);
     // shooterBottomPortMotor.set(0);
+    shooterTopPortMotor.setVoltage(0);
+    shooterBottomPortMotor.setVoltage(0);
   }
 }
