d74f4146e4524ace54781462fc1c3b093777e19d
Cpixel151 <milostories151@gmail.com>
2026-03-15 15:45:55 -0400

    8 foot

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -36,42 +36,39 @@ public class ShooterSubsystem extends SubsystemBase {
   private final SparkFlexConfig kickConfig;
   private final SparkClosedLoopController kickClosedLoopController;
 
-
   public ShooterSubsystem() {
 
     SmartDashboard.putNumber("Velocity Top", 0);
     SmartDashboard.putNumber("Velocity Bottom", 0);
     SmartDashboard.putNumber("Current Limit", 0);
-    setVelocityTop = 4200;
-    setVelocityBottom = 4200;
+    setVelocityTop = 4100;
+    setVelocityBottom = 4100;
     setVelocityKicker = 3000;
-    
 
     shootMotorTop = new SparkFlex(13, MotorType.kBrushless);
     shootMotorBottom = new SparkFlex(15, MotorType.kBrushless);
     shootFollowTop = new SparkFlex(14, MotorType.kBrushless);
     shootFollowBottom = new SparkFlex(16, MotorType.kBrushless);
-    kickMotor = new SparkFlex(35, MotorType.kBrushless);
+    kickMotor = new SparkFlex(31, MotorType.kBrushless);
 
     bFollowerConfig = new SparkFlexConfig();
     tFollowerConfig = new SparkFlexConfig();
     tShootConfig = new SparkFlexConfig();
     bShootConfig = new SparkFlexConfig();
-    //commented to add when we know kicker runs
+    // commented to add when we know kicker runs
     kickConfig = new SparkFlexConfig();
-    
 
     tShootClosedLoopController = shootMotorTop.getClosedLoopController();
     bSparkClosedLoopController = shootMotorBottom.getClosedLoopController();
-    //commented to add when we know kicker runs
+    // commented to add when we know kicker runs
     kickClosedLoopController = kickMotor.getClosedLoopController();
 
     tShootConfig.smartCurrentLimit(40);
     bShootConfig.smartCurrentLimit(40);
-    //commented to add when we know kicker runs
+    // commented to add when we know kicker runs
     kickConfig.smartCurrentLimit(40);
 
-    //This might cause issues
+    // This might cause issues
     // tShootConfig.encoder.positionConversionFactor(1);
     // tShootConfig.encoder.positionConversionFactor(1);
 
@@ -111,17 +108,17 @@ public class ShooterSubsystem extends SubsystemBase {
         // kV is now in Volts, so we multiply by the nominal voltage (12V)
         .kV(12.0 / 5767, ClosedLoopSlot.kSlot1);
 
-    //commented to add when we know kicker runs
-    //0.00039
-    //0.0000016
+    // commented to add when we know kicker runs
+    // 0.00039
+    // 0.0000016
     kickConfig
         .closedLoop
         .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
         // Set PID values for position control. We don't need to pass a closed loop
         // slot, as it will default to slot 0.
-        //0.00039
+        // 0.00039
         .p(0.00034)
-        //0.000002
+        // 0.000002
         .i(0.0000009)
         .d(0)
         .outputRange(-1, 1)
@@ -142,39 +139,33 @@ public class ShooterSubsystem extends SubsystemBase {
         bShootConfig,
         com.revrobotics.ResetMode.kNoResetSafeParameters,
         com.revrobotics.PersistMode.kPersistParameters);
-    
-    //commented to add when we know kicker runs
+
+    // commented to add when we know kicker runs
     kickMotor.configure(
-      kickConfig,
-      com.revrobotics.ResetMode.kNoResetSafeParameters,
-      com.revrobotics.PersistMode.kPersistParameters
-    );
+        kickConfig,
+        com.revrobotics.ResetMode.kNoResetSafeParameters,
+        com.revrobotics.PersistMode.kPersistParameters);
   }
 
   // Shoot balls. None adjustable velocity
   public void Shoot() {
 
-    
     tShootClosedLoopController.setSetpoint(setVelocityTop, ControlType.kVelocity);
     bSparkClosedLoopController.setSetpoint(setVelocityBottom, ControlType.kVelocity);
-    
   }
 
   // Used to kick the balls up from the storage up into the shooter
   public void KickBalls() {
-    //kickMotor.set(1);
-    //commented to add when we know kicker runs
+    // kickMotor.set(1);
+    // commented to add when we know kicker runs
     kickClosedLoopController.setSetpoint(setVelocityKicker, ControlType.kVelocity);
-    
   }
 
   public void StopShoot() {
 
-
     shootMotorTop.setVoltage(0);
     shootMotorBottom.setVoltage(0);
     kickMotor.setVoltage(0);
-    
   }
 
   public double GetRPM() {
