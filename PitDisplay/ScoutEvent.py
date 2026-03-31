8d23aaa7293a4241f16fd93105dddc7c09df2f87
NoobinOut <110687587+NoobinOut@users.noreply.github.com>
2026-03-31 15:46:24 -0400

    make it threaded

--- PitDisplay/ScoutEvent.py
+++ PitDisplay/ScoutEvent.py
@@ -4,12 +4,151 @@ from oauth2client.service_account import ServiceAccountCredentials
 import time
 import statbotics
 import math
+import threading
+
+global outputmaster
+
+outputmaster = []
+
+def scoutMatch(offset, offsetmax, teamnumbers, typematch, match_number, EventName, data):
+    global outputmaster
+    #print("Formatted: " + str(len(data)) + " matches, and am about to create: " + str(len(teamnumbers)) + " entries")
+    print("Thread: " + str(offset) + " to " + str(offsetmax) + " started")
+
+    #print(teamnumbers)
+    #print(matchkey)
+
+    #print("Connected, Connecting to Statbotics")
+
+    sb = statbotics.Statbotics()
+
+    #print("Connected, starting data feed")
+
+    # loop through all team numbers we have
+    for i in range(offset,offsetmax):
+
+        try:
+            # try to get further match data
+            #print("Match Data sent for team: " + str(teamnumbers[i]) + " and match: " + str(matchkey[i]))
+            data = sb.get_team_match(teamnumbers[i],matchkey[i])
+            #print("Match Data recieved from Statbotics")
+        except Exception as e:
+            print("Failed to get match data. Error is: " + str(e))
+            continue
+
+        # get match breakdown
+        breakdown = data['epa']['breakdown']
+        color = data['alliance']
+
+        #print(data)
+
+        if breakdown['auto_tower'] > 0:
+            ClimerAutoStatus = 1
+            ClimberAuto = "Yes, successfully"
+        else:
+            ClimerAutoStatus = 0
+            ClimberAuto = ""
+
+        if breakdown['endgame_tower'] >= 30:
+            EndgameL1 = 0
+            EndgameL2 = 0
+            EndgameL3 = 1
+        elif breakdown['endgame_tower'] >= 20:
+            EndgameL1 = 0
+            EndgameL2 = 1
+            EndgameL3 = 0
+        elif breakdown['endgame_tower'] >= 10:
+            EndgameL1 = 1
+            EndgameL2 = 0
+            EndgameL3 = 0
+        else:
+            EndgameL1 = 0
+            EndgameL2 = 0
+            EndgameL3 = 0
+
+        if breakdown['endgame_fuel'] > 0:
+            EndgameStatus = "Continue Shooting"
+        else:
+            EndgameStatus = ""
+
+        #print(breakdown)
+
+        output = [
+            "", # Timestamp
+            "Beep Boop 9000", # Your (REAL) Name
+            teamnumbers[i], # Enter assigned Robot's Team Number
+            typematch[i], # Match Type
+            match_number[i], # Match Number
+            "", # Did not compete: Click the button below if the robot did not make it onto the field. Otherwise, just click "Next".
+            "", # Competition
+            "", # Unique Match Id
+            "", # Unique Match ID 2.0
+            color, # Which color alliance is your robot on?
+            ClimberAuto, # Does your robot climb to L1?
+            math.floor(breakdown['auto_fuel']), # How many shots did your robot attempt during AUTO?
+            "", # Does your robot refuel during AUTO?
+            "", # How many cycles does your robot go through (how many times did it return to shoot fuel)?
+            "", # Where can your robot pick up fuel cells from?	What does your robot do during endgame?
+            "", # What does your robot do during endgame?
+            "",
+            "", # How many cycles (if any) did your robot complete during Endgame?
+            totalBoard[i], # What was the TOTAL SCORE of your alliance as seen on the board?
+            penaltyBoard[i], # How many penalty points did your alliance collect?
+            "", # Was this robot disabled at any point during teleop/endgame?
+            "", # Identify the type of robot you're scouting.
+            "", # Did your robot play defense?
+            math.floor(breakdown['auto_tower']), # Auto Hang Points
+            math.floor(breakdown['auto_points']), # Auto Total Score
+            math.floor(breakdown['teleop_points']), # Teleop Points
+            math.floor(breakdown['endgame_tower']), # Endgame Hang Points
+            math.floor(breakdown['endgame_points']), # Endgame Total
+            "", # TOTAL	Disablement
+            "", # Nothing
+            "", # Unique Match ID
+            ClimerAutoStatus, # Auto L1
+            EndgameL3, # Endgame L3
+            EndgameL2, # Endgame L2
+            EndgameL1, # Endgame L1
+            "", # Defense?
+            math.floor(breakdown['total_points']) # Robot Total Score
+            ]
+
+        for h in range(0,len(output)):
+            try:
+                if output[h] < 0:
+                    output[h] = 0
+            except:
+                pass
+
+        #print("Sending Match Data To Google Sheets")
+        #try:
+        #    worksheet.insert_row(output, inputCol)
+        #except Exception as e:
+        #    print(str(e))
+        #    print("Waiting abit")
+        #    print(str(output))
+        #    time.sleep(5)
+        #    continue
+
+        outputmaster.append(output)
+
+        #exit()
+
+        #print("Waiting abit")
+        print(str(output))
+        print(str(100*((i+1)/len(teamnumbers)))[:4] + "% Done | " + str(i+1) + "/" + str(len(teamnumbers)))
+        time.sleep(2)
+
+
+
+
+
 
 w = open("BlueAllianceAPI.txt", 'r')
 Authkey = w.read()
 w.close()
 
-eventkey = "2026ncwak"
+eventkey = "2026ncpem"
 
 matchkey = []
 teamnumbers = []
@@ -33,10 +172,15 @@ EventName = resp.json()['name']
 
 #exit()
 
+threads = []
+
 print("Got recieved formatting")
 
 # loop through all matches we are given
 for i in range(0,len(data)):
+
+    numberofteams = 0
+
     if data[i]['winning_alliance'] == "":
         try:
             print("Match: " + str(data[i]['match_number']) + " has not finished yet")
@@ -49,12 +193,13 @@ for i in range(0,len(data)):
     #    print("Already Scouted match: " + str(data[i]['match_number']))
     #    continue
 
-    if data[i]['comp_level'] != "sf" and data[i]['comp_level'] != "f":
-        print("Already Scouted match: " + str(data[i]['match_number']))
-        continue
+    #if data[i]['comp_level'] != "sf" and data[i]['comp_level'] != "f":
+    #    print("Already Scouted match: " + str(data[i]['match_number']))
+    #    continue
 
     # loop through all blue alliances we are given
     for j in range(0,len(data[i]['alliances']['blue']['team_keys'])):
+        numberofteams += 1
 
         # fill in team number, matchkey, board-score, alliance penalty, match number, and type
         teamnumbers.append(int(data[i]['alliances']['blue']['team_keys'][j].replace("frc","").strip()))
@@ -82,6 +227,7 @@ for i in range(0,len(data)):
 
     # loop through all red alliances we are given
     for j in range(0,len(data[i]['alliances']['red']['team_keys'])):
+        numberofteams += 1
 
         # fill in team number, matchkey, board-score, alliance penalty, match number, and type
         teamnumbers.append(int(data[i]['alliances']['red']['team_keys'][j].replace("frc","").strip()))
@@ -107,10 +253,22 @@ for i in range(0,len(data)):
         else:
             typematch.append("")
 
-print("Formatted: " + str(len(data)) + " matches, and am about to create: " + str(len(teamnumbers)) + " entries")
+    try:
+        print("Setting up thread: " + str(i))
+        threads.append(threading.Thread(target=scoutMatch, args=(i*6, (i*6)+numberofteams, teamnumbers, typematch, match_number, EventName, data)))
+    except:
+        print("Failed to create thread: " + str(i))
+    #scoutMatch(i*6, (i*6)+numberofteams, teamnumbers, typematch, match_number, EventName, data)
 
-#print(teamnumbers)
-#print(matchkey)
+print("All threads created starting")
+for i in range(0, len(threads)):
+    threads[i].start()
+for i in range(0, len(threads)):
+    threads[i].join()
+
+#print(str(outputmaster))
+
+print("All threads done")
 
 # Define the scope
 scope = [
@@ -119,12 +277,12 @@ scope = [
 ]
 
 # Authenticate with credentials
-credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('creds2.json', scope)
 client = gspread.authorize(credentials)
 
 # Open the Google Sheet
-sheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1-AsEmhEqbABNO2hv2vS37AGeSQiSQW0leekwXtnfDy8/edit?resourcekey=&gid=780377629#gid=780377629')
-#sheet = client.open_by_url('https://docs.google.com/spreadsheets/d/14U0JZ4Te7u0Prj2kdQF14Q0PRz7UKFYLrVq7CF0W7hI/edit?gid=1531398796#gid=1531398796')
+#sheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1-AsEmhEqbABNO2hv2vS37AGeSQiSQW0leekwXtnfDy8/edit?resourcekey=&gid=780377629#gid=780377629')
+sheet = client.open_by_url('https://docs.google.com/spreadsheets/d/14U0JZ4Te7u0Prj2kdQF14Q0PRz7UKFYLrVq7CF0W7hI/edit?gid=1531398796#gid=1531398796')
 
 worksheet = sheet.worksheet("Raw Data")
 
@@ -132,121 +290,7 @@ cells = worksheet.col_values(1)
 
 inputCol = len(cells) + 1
 
-print("Connected, Connecting to Statbotics")
-
-sb = statbotics.Statbotics()
-
-print("Connected, starting data feed")
-
-# loop through all team numbers we have
-for i in range(0,len(teamnumbers)):
-
-    try:
-        # try to get further match data
-        print("Match Data sent for team: " + str(teamnumbers[i]) + " and match: " + str(matchkey[i]))
-        data = sb.get_team_match(teamnumbers[i],matchkey[i])
-        print("Match Data recieved from Statbotics")
-    except Exception as e:
-        print("Failed to get data. Error is: " + str(e))
-        continue
-
-    # get match breakdown
-    breakdown = data['epa']['breakdown']
-    color = data['alliance']
-
-    #print(data)
-
-    if breakdown['auto_tower'] > 0:
-        ClimerAutoStatus = 1
-        ClimberAuto = "Yes, successfully"
-    else:
-        ClimerAutoStatus = 0
-        ClimberAuto = ""
-
-    if breakdown['endgame_tower'] >= 30:
-        EndgameL1 = 0
-        EndgameL2 = 0
-        EndgameL3 = 1
-    elif breakdown['endgame_tower'] >= 20:
-        EndgameL1 = 0
-        EndgameL2 = 1
-        EndgameL3 = 0
-    elif breakdown['endgame_tower'] >= 10:
-        EndgameL1 = 1
-        EndgameL2 = 0
-        EndgameL3 = 0
-    else:
-        EndgameL1 = 0
-        EndgameL2 = 0
-        EndgameL3 = 0
-
-    if breakdown['endgame_fuel'] > 0:
-        EndgameStatus = "Continue Shooting"
-    else:
-        EndgameStatus = ""
-
-    #print(breakdown)
-
-    output = [
-        "", # Timestamp
-        "Beep Boop 9000", # Your (REAL) Name
-        teamnumbers[i], # Enter assigned Robot's Team Number
-        typematch[i], # Match Type
-        match_number[i], # Match Number
-        "", # Did not compete: Click the button below if the robot did not make it onto the field. Otherwise, just click "Next".
-        EventName, # Competition
-        "", # Unique Match Id
-        "", # Unique Match ID 2.0
-        color, # Which color alliance is your robot on?
-        ClimberAuto, # Does your robot climb to L1?
-        math.floor(breakdown['auto_fuel']), # How many shots did your robot attempt during AUTO?
-        "", # Does your robot refuel during AUTO?
-        "", # How many cycles does your robot go through (how many times did it return to shoot fuel)?
-        "", # Where can your robot pick up fuel cells from?	What does your robot do during endgame?
-        "", # What does your robot do during endgame?
-        "",
-        "", # How many cycles (if any) did your robot complete during Endgame?
-        totalBoard[i], # What was the TOTAL SCORE of your alliance as seen on the board?
-        penaltyBoard[i], # How many penalty points did your alliance collect?
-        "", # Was this robot disabled at any point during teleop/endgame?
-        "", # Identify the type of robot you're scouting.
-        "", # Did your robot play defense?
-        math.floor(breakdown['auto_tower']), # Auto Hang Points
-        math.floor(breakdown['auto_points']), # Auto Total Score
-        math.floor(breakdown['teleop_points']), # Teleop Points
-        math.floor(breakdown['endgame_tower']), # Endgame Hang Points
-        math.floor(breakdown['endgame_points']), # Endgame Total
-        "", # TOTAL	Disablement
-        "", # Nothing
-        "", # Unique Match ID
-        ClimerAutoStatus, # Auto L1
-        EndgameL3, # Endgame L3
-        EndgameL2, # Endgame L2
-        EndgameL1, # Endgame L1
-        "", # Defense?
-        math.floor(breakdown['total_points']) # Robot Total Score
-        ]
-
-    for h in range(0,len(output)):
-        try:
-            if output[h] < 0:
-                output[h] = 0
-        except:
-            pass
-
-    print("Sending Match Data To Google Sheets")
-    try:
-        worksheet.insert_row(output, inputCol)
-    except Exception as e:
-        print(str(e))
-        print("Waiting abit")
-        print(str(100*((i+1)/len(teamnumbers)))[:4] + "% Done | " + str(i+1) + "/" + str(len(teamnumbers)))
-        time.sleep(5)
-        continue
-
-    #exit()
-
-    print("Waiting abit")
-    print(str(100*((i+1)/len(teamnumbers)))[:4] + "% Done | " + str(i+1) + "/" + str(len(teamnumbers)))
-    time.sleep(1)
-
+for i in range(0,len(outputmaster)):
+    print("Sending row: " + str(i))
+    worksheet.insert_row(outputmaster[i], inputCol)
+    time.sleep(2)
