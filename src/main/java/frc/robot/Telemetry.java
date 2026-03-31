cfc0ac5abd7c2fadf75c983436bdb024359dbe31
NoobinOut <110687587+NoobinOut@users.noreply.github.com>
2026-03-31 18:34:35 -0400

    Nuke Swerve Telemetry

--- src/main/java/frc/robot/Telemetry.java
+++ src/main/java/frc/robot/Telemetry.java
@@ -32,8 +32,8 @@ public class Telemetry {
    * @param maxSpeed Maximum speed in meters per second
    */
   public Telemetry(double maxSpeed) {
+    SignalLogger.stop();
     MaxSpeed = maxSpeed;
-    SignalLogger.start();
 
     /* Set up the module state Mechanism2d telemetry */
     for (int i = 0; i < 4; ++i) {
@@ -167,17 +167,6 @@ public class Telemetry {
     driveModulePositions.set(state.ModulePositions);
     driveTimestamp.set(state.Timestamp);
     driveOdometryFrequency.set(1.0 / state.OdometryPeriod);
-
-    /* Also write to log file */
-    SignalLogger.writeStruct("DriveState/Pose", Pose2d.struct, state.Pose);
-    SignalLogger.writeStruct("DriveState/Speeds", ChassisSpeeds.struct, state.Speeds);
-    SignalLogger.writeStructArray(
-        "DriveState/ModuleStates", SwerveModuleState.struct, state.ModuleStates);
-    SignalLogger.writeStructArray(
-        "DriveState/ModuleTargets", SwerveModuleState.struct, state.ModuleTargets);
-    SignalLogger.writeStructArray(
-        "DriveState/ModulePositions", SwerveModulePosition.struct, state.ModulePositions);
-    SignalLogger.writeDouble("DriveState/OdometryPeriod", state.OdometryPeriod, "seconds");
 
     /* Telemeterize the pose to a Field2d */
     fieldTypePub.set("Field2d");
