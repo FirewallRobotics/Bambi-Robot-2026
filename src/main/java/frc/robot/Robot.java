c76351a1cc2eaece73818dfe46fc50600e8fe944
NoobinOut <110687587+NoobinOut@users.noreply.github.com>
2026-04-06 18:45:50 -0400

    Update libraries

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,8 +8,6 @@ import com.ctre.phoenix6.HootAutoReplay;
 import edu.wpi.first.net.PortForwarder;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
